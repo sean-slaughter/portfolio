ceb28e344260a59d60253a6ccc6a027cd29f1ecd
sean-slaughter <sthompson2822@gmail.com>
2020-10-26 16:11:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello. My name is Sean Thompson and I am a full stack software developer.
+        Let's work together on something great.
     </div>
   );
 }
