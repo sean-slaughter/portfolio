b5e480ba65d8edef776f7e2ceac011799a101d9e
sean-slaughter <sthompson2822@gmail.com>
2020-10-27 17:51:25 -0500

    Add color theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-        Hello. My name is Sean Thompson and I am a full stack software developer.
-        Let's work together on something great.
+   
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+     main: "#d48c34"
+    },
+    secondary: {
+      main: "#353130"
+    }
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
