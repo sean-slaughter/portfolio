2b9d29a973ec2fb8749ca9d427c21d443c8cbe97
sean-slaughter <sthompson2822@gmail.com>
2020-10-28 17:56:22 -0500

    Add photo to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import HomeImage from "../components/HomeImage";
 import { Link } from "react-router-dom";
 
@@ -8,6 +8,13 @@ export default function Home(props) {
   return (
       <Container>
         <motion.div exit={{x: -2000}} key="home"transition={{ease: "easeInEaseOut"}} className="home-container">
+            <motion.div
+             initial={{ x: "-200vw" }}
+             animate={{ x: 0 }}
+             transition={{ delay: 0.6, type: "spring", damping: 15 }}
+            >
+              <Image src="resume.jpeg" roundedCircle width={225}/>
+            </motion.div>
             <motion.p
             class="row home-text text-center"
             initial={{ x: "-200vw" }}
@@ -22,14 +29,14 @@ export default function Home(props) {
             animate={{ x: 0 }}
             transition={{ delay: 1.4, type: "spring", damping: 15 }}
             >
-                And I'm a Computer Science student and <br/> Full Stack Software Developer based out of Milwaukee, WI.
+                And I'm a Computer Science student and <br/> software developer based out of Milwaukee, WI.
             </motion.p>
             <br/>
             <motion.p
             className="home-description text-center"
             initial={{opacity: 0}}
             animate={{opacity: 1}}
-            transition={{delay: 2.6, duration: 1}}
+            transition={{delay: 2.8, duration: 1}}
             > 
             Some of my current favorite technologies include Java, ReactJS, Rails, PostgreSQL, and MongoDB.
             </motion.p>
@@ -37,7 +44,7 @@ export default function Home(props) {
             className="home-description text-center"
             initial={{opacity: 0}}
             animate={{opacity: 1}}
-            transition={{delay: 3.3, duration: 1}}
+            transition={{delay: 4, duration: 1}}
             > 
             Lets connect and create something great together!
             </motion.p>
@@ -45,16 +52,16 @@ export default function Home(props) {
         <motion.div
          initial={{opacity: 0}}
          animate={{opacity: 1}}
-         transition={{delay: 3.3, duration: 1}}>
+         transition={{delay: 4, duration: 1}}>
         <Row className="justify-content-center home-buttons">
           <Link to='/resume'>
-            <Button variant="outline-dark" style={{margin: 20}}size="lg">Resume</Button>
+            <Button variant="outline-dark" style={{marginLeft: 20, marginRight: 20}}size="lg">Resume</Button>
           </Link>
           <Link to='/projects'>
-            <Button variant="outline-dark" style={{margin: 20}}size="lg">Projects</Button>
+            <Button variant="outline-dark" style={{marginLeft: 20, marginRight: 20}}size="lg">Projects</Button>
           </Link>
           <Link to='/contact'>
-            <Button variant="outline-dark" style={{margin: 20}}size="lg">Contact</Button>
+            <Button variant="outline-dark" style={{marginLeft: 20, marginRight: 20}}size="lg">Contact</Button>
           </Link>
         </Row>
         </motion.div>
