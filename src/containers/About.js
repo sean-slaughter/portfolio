38bc20f0ad955657bab65da436f6b63dc706f606
sean-slaughter <sthompson2822@gmail.com>
2020-10-28 18:49:03 -0500

    add nina and luna photos to about

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Modal, Row } from 'react-bootstrap'
+import { Button, Image, Modal, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import { FaGithub, FaMedium, FaLinkedin } from 'react-icons/fa'
 export default function About() {
@@ -28,15 +28,22 @@ export default function About() {
                 that I have.
                 </p>
                 <p className="modal-text">
-                Anyways, here are some links to my socials and then enough about me, let's talk about MY DOGS.
+                Anyways, here are some links to my socials and then enough about me, let's talk about MY DOGS. Because that is obviously why you came here.
                 </p>
                 <Row className="justify-content-center">
                     <Button variant="outline-dark" style={{margin:20}} href="https://github.com/sean-slaughter">My Code:<br/><FaGithub size={50}/></Button>
                     <Button variant="outline-dark" style={{margin:20}}href="https://medium.com/@seanslaughterthompson">My Blog:<br/><FaMedium size={50}/></Button>
-                    <Button variant="outline-dark" style={{margin:20}}href="https://github.com/sean-slaughter">My Network:<br/><FaLinkedin size={50}/></Button>
+                    <Button variant="outline-dark" style={{margin:20}}href="https://linkedin.com/in/seanslaughterthompson">My Network:<br/><FaLinkedin size={50}/></Button>
                 </Row>
-
-
+                <p className="modal-text text-center" style={{marginTop:25}}>
+                    MY DOGS:
+                </p>
+                <Row className="justify-content-center">
+                    <Image src="/nina.jpg" width={400} rounded/>
+                </Row>
+                <p className="modal-text text-center" style={{marginTop: 30}}>
+                    This is Nina. She enjoys balls, tasty food, and carpeted areas. She is terrified of hardwood floors and spends most days sleeping.
+                </p>
             </Modal.Body>
             <Modal.Footer>
                 <Link to="/">
